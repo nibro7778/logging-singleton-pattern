b2990bc9163063f5f69318a4556bbd47d11716da
Niraj Trivedi <mailme7778@gmail.com>
2018-06-23 17:14:19 +1000

    Added singleton pattern and log message

---
+++ singleton-logging/Logger/Log.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Logger
+{
+    public class Log : ILog
+    {
+        /// <summary>
+        /// Static field to stored instance of the singleton class
+        /// </summary>
+        private static Log _instance;
+
+        /// <summary>
+        /// Private construct to restrict user to instance outside the class
+        /// </summary>
+        private Log() { }
+
+        /// <summary>
+        /// GetInstance method which create new instance if its null, and return instance  
+        /// </summary>
+        public static Log GetInstance => _instance ?? (_instance = new Log());
+
+
+        public void LogException(string message)
+        {
+            // You can write your code to logging message to files or external data source file
+            throw new System.NotImplementedException();
+        }
+    }
+}
